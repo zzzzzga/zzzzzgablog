423ce75cd5328bfadfb67d946f6ca6b224f61d1f
chenq <c1311949161@gmail.com>
2017-04-27 16:29:33 +0800

    bug modified

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const checkLogin = require('../middlewares/check').checkLogin;
 const PostModel = require('../models/posts');
+const CommentModel = require('../models/comments')
 
 // GET /posts 所有用户或者特定用户的文章页
 //   eg: GET /posts?author=xxx
